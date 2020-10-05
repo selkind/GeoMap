e911709a10622298587d49fa061a20fe8f90a640
Sam Elkind <samuel.elkind@gmail.com>
2020-10-05 02:53:55 -0600

    create script to generate field glossary

---
+++ documentation/build_attribute_glossary.py
@@ -0,0 +1,14 @@
+import os
+import geopandas as pgd
+import pandas as pd
+
+import mdutils
+
+
+def main():
+    cwd = os.getcwd()
+    geomap_path = os.path.join(cwd, "data", "ATA_SCAR_GeoMAP_geology.gdb")
+
+
+if __name__ == "__main__":
+    main()
