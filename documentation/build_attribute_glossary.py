539e4cb472fd08f17ec7d4a691b83da38a170dbb
Sam Elkind <samuel.elkind@gmail.com>
2020-10-05 06:13:32 -0600

    refactor into functions to clean up. change md output

--- documentation/build_attribute_glossary.py
+++ documentation/build_attribute_glossary.py
@@ -1,9 +1,44 @@
+import os
 import geopandas as gpd
 import pandas as pd
 import file_paths as fp
 import fields
 
 import mdutils
+
+
+def create_output(record, field_name):
+    source = record['source_of_vals'].iloc[0]
+    formatting = record['value_formatting'].iloc[0]
+    metadata_link = record['field_metadata_link'].iloc[0]
+    # field_vals = record['Field Values'].iloc[0]
+    restrictions = record['Field value restrictions'].iloc[0]
+    return {"Source of Values": source,
+            "Value Format": formatting,
+            "Metadata Link": metadata_link,
+            "Field Values": f"{os.path.join(os.path.basename(fp.FIELD_VALS_DIR), field_name)}_values.md",
+            "Field Value Restrictions": restrictions}
+
+
+def format_output(descr_header, value):
+    # this is a sloppy handling of blanks because Pandas reads "None" as nan. There's probably a way to change this
+    if value == "blank":
+        value == "None"
+
+    elif descr_header in ["Metadata Link", "Field Value Restrictions"] and "http" in value:
+        value = mdutils.MdUtils.new_inline_link(value, value)
+    elif descr_header == "Field Values":
+        value = mdutils.MdUtils.new_inline_link(value, "List of Values")
+    return value
+
+
+def create_stats(value_counts):
+    stats = {}
+    stats["Unique Values"] = len(value_counts)
+    if len(value_counts) > 0:
+        stats["Most frequently occurring value"] = value_counts.index[0]
+        stats["Number of values with a single occurrence"] = len([k for k in value_counts if k == 1])
+    return stats
 
 
 def main():
@@ -22,36 +57,21 @@ def main():
 
         condition = field_descr['field_name'] == i
         record = field_descr.loc[condition]
-        source = record['source_of_vals'].iloc[0]
-        formatting = record['value_formatting'].iloc[0]
-        metadata_link = record['field_metadata_link'].iloc[0]
-        field_vals = record['Field Values'].iloc[0]
-        restrictions = record['Field value restrictions'].iloc[0]
-        output = {"Source of Values": source,
-                  "Value Format": formatting,
-                  "Metadata Link": metadata_link,
-                  "Field Values": field_vals,
-                  "Field Value Restrictions": restrictions}
+        output = create_output(record, i)
 
         mdfile.new_header(1, i)
         for j in output:
-            if output[j] == "blank":
-                output[j] = "None"
-
-            if j in ["Metadata Link", "Field Values", "Field Value Restrictions"]:
-                output[j] == mdfile.new_inline_link(output[j], output[j])
-            mdfile.new_header(2, f"{j}: {output[j]}")
+            output[j] = format_output(j, output[j])
+            mdfile.new_header(2, f"{j}:")
+            mdfile.new_header(3, output[j])
 
         mdfile.new_header(2, "Descriptive Statistics:")
-        stats = {}
-        stats["Unique Values"] = len(value_counts)
-        if len(value_counts) > 0:
-            stats["Most frequently occurring value"] = value_counts.index[0]
-            stats["Number of values with a single occurrence"] = len([k for k in value_counts if k == 1])
+
+        stats = create_stats(value_counts)
 
         mdfile.new_list([f"{k}: {stats[k]}" for k in stats])
 
-    mdfile.new_table_of_contents("Table of Contents", depth=3)
+    mdfile.new_table_of_contents()
     mdfile.create_md_file()
 
 
