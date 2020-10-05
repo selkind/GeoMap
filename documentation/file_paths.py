af7c043f3027ecebc38f3375667dcc6f7cc92c9b
Sam Elkind <samuel.elkind@gmail.com>
2020-10-05 03:40:49 -0600

    add glossary markdown path

--- documentation/file_paths.py
+++ documentation/file_paths.py
@@ -12,7 +12,8 @@ CULTURE_PATH = os.path.join(DATA_DIR, "ATA_SCAR_GeoMAP_culture.gdb")
 
 FIELD_VALS_DIR = os.path.join(DOC_DIR, "field_values")
 
+GLOSSARY_PATH = os.path.join(DOC_DIR, "field_glossary.md")
+
 for i in [FIELD_DESCR_PATH, GEOL_PATH, CULTURE_PATH]:
     if not os.path.exists(i) and not os.path.isfile(i):
         raise FileNotFoundError(f"{i} does not exist")
-
