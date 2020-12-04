f559611687b88ad070358cdde709766e06ffa806
Samuel Elkind <samuel.elkind@gmail.com>
2020-12-04 16:35:02 -0700

    bibtex formatting correct. Author parsing untested

--- build/build_bibliography.py
+++ build/build_bibliography.py
@@ -5,15 +5,17 @@ import geopandas as gpd
 # import mdutils
 from build.file_paths import GEOL_PATH
 
+REMOVED_CHARS = " .,;"
+
 
 def make_citations():
     geomap = gpd.read_file(GEOL_PATH, layer="ATA_sources_poly")
     published = geomap[geomap["PUBTYPE"].isin(["Published paper", "Published map"])]
-    print(published.columns)
-
+    # print(published.columns)
     citations = {}
-    for i in range(published.shape[0]):
-        citations[published.iloc[i]['IDENTIFIER']] = make_mla_markdown_citation(published.iloc[i])
+    # for i in range(published.shape[0]):
+    for i in range(4):
+        citations[published.iloc[i]['IDENTIFIER']] = build_bibtex(published.iloc[i])
 
     return citations
 
@@ -24,8 +26,45 @@ def make_mla_markdown_citation(record):
     return authors
 
 
+def split_name(author):
+    name = author.split(" ")
+    last_name = name[0].strip(REMOVED_CHARS)
+    if len(name) > 1:
+        initials = ""
+        for i in name[1:]:
+            for j in REMOVED_CHARS:
+                i = i.replace(j, "")
+            initials += i
+
+        return (last_name, initials)
+    return (last_name, None)
+
+
+def build_bibtex(record):
+    author_list = split_authors(record['AUTHORS'])
+
+    authors = ""
+    for i in author_list[:-1]:
+        author = split_name(i)
+        authors += f"{author[0]}, {author[1]} and " if author[1] else f"{author[0]} and "
+    last_author = split_name(author_list[-1])
+    authors += f"{last_author[0]}, {last_author[1]}" if last_author[1] else last_author[0]
+
+    identifier = split_name(author_list[0])[0]
+    identifier += str(int(record['YEAR']))
+    identifier += record['TITLE'].split(" ")[0]
+
+    return f"@article{{{identifier},\n" \
+           f"  title={{{record['TITLE']}}},\n"\
+           f"  authors={{{authors}}},\n"\
+           f"  journal={{{record['PUBLICATION']}}},\n"\
+           f"  year={{{int(record['YEAR'])}}}\n"\
+           f"}}"
+
+
 def split_authors(authors):
-    # list is ordered so that entries with multiple delimiters will have the more likely delimiter chosen. ',' can be a delimiter for 
+    # list is ordered so that entries with multiple delimiters will have the more likely delimiter chosen. ','
+    # can be a delimiter for
     # first and last name, whereas ampersand and semi-colon will not be and thus have higher 'priority'
     delimiters = ("&", ";", ",")
     delimiter = None
@@ -60,7 +99,7 @@ def format_mla_authors(split_authors):
 
 
 def main():
-    test = make_citations()
+    print(make_citations())
 
 
 if __name__ == "__main__":
