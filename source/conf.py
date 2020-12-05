51af282f77b390e2b6a0a316ffc9e9f555a6a83d
Samuel Elkind <samuel.elkind@gmail.com>
2020-12-05 15:07:46 -0700

    try xelatex engine

--- source/conf.py
+++ source/conf.py
@@ -68,3 +68,5 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+latex_engine = 'xelatex'
