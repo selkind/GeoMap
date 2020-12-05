82a12f6cb5fde731e80c32a6c1fd77b7bfe4244d
Samuel Elkind <samuel.elkind@gmail.com>
2020-12-05 13:28:47 -0700

    update site directory

--- build/file_paths.py
+++ build/file_paths.py
@@ -3,7 +3,7 @@ import os
 BUILD_DIR = os.path.dirname(os.path.realpath(__file__))
 
 BASE_DIR = os.path.dirname(BUILD_DIR)
-SITE_DIR = os.path.join(BASE_DIR, "docs")
+SITE_DIR = os.path.join(BASE_DIR, "source")
 
 FIELD_DESCR_PATH = os.path.join(BUILD_DIR, "field_descr_data.csv")
 
@@ -14,7 +14,7 @@ CULTURE_PATH = os.path.join(DATA_DIR, "ATA_SCAR_GeoMAP_culture.gdb")
 FIELD_VALS_DIR = os.path.join(SITE_DIR, "field_values")
 
 GLOSSARY_PATH = os.path.join(SITE_DIR, "field_glossary.md")
-BIB_PATH = os.path.join(SITE_DIR, "bibliography.md")
+BIB_PATH = os.path.join(SITE_DIR, "works_referenced.md")
 
 for i in [FIELD_DESCR_PATH, GEOL_PATH, CULTURE_PATH]:
     if not os.path.exists(i) and not os.path.isfile(i):
