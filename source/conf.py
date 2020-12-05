d723ef3708f1ee347ddec9611b71840c65210abb
Samuel Elkind <samuel.elkind@gmail.com>
2020-12-05 14:28:21 -0700

    add arbitrary comment to force rebuild

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
+#arbitrary comment
 # -- Project information -----------------------------------------------------
 
 project = 'GeoMAP'
