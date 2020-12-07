fdeb7e468f93330b3bee870e6c65629d2522adcf
Samuel Elkind <samuel.elkind@gmail.com>
2020-12-07 12:11:31 -0700

    add "and" test cases for split_author

--- tests/test_build_bibliography.py
+++ tests/test_build_bibliography.py
@@ -15,15 +15,17 @@ from src.build_bibliography import split_authors, split_name
     ("Skinner D.N.B.; Waterhouse B.C.; Brehaut G.; Sullivan K,", ['Skinner D.N.B.', 'Waterhouse B.C.', 'Brehaut G.',
      'Sullivan K.']),
     ("Ballance P.F.", ['Ballance P.F.']),
-    ("Barrett P.J. et al. in Cooper A.K. et al. (eds)", ['Barrett P.J.', 'et al.']),
     ("Hamilton R.J.; Luyendyk B.P.; Sorlien C.", ['Hamilton R.J.', 'Luyendyk B.P.', 'Sorlien C.']),
     ("Rowell A.J., Rees M et al.", ['Rowell A.J.', 'Rees M', 'et al.']),
     ("Capponi G., Casnedi R., Castelli D., Flotmann T. et al.", ['Capponi G.', 'Casnedi R.', 'Castelli D.',
      'Flotmann T.', 'et al.']),
     ("McGregor V.R.; Wade F.A.", ['McGregor V.R.', 'Wade F.A.']),
+    ("Barrett P.J. et al. in Cooper A.K. et al. (eds)", ['Barrett P.J.', 'et al.']),
     ('foo & bar in cooper et al.', ['foo', 'bar']),
     ('foo G. & bar K. in cooper et al.', ['foo G.', 'bar K.']),
     ('foo G. in cooper et al.', ['foo G.']),
+    ('foo G. and bar K. in cooper et al.', ['foo G.', 'bar K.']),
+    ("McElroy C.T. and Rose G.", ['McElroy C.T.', 'Rose G.']),
 ])
 def test_split_authors(test_input, expected):
     assert split_authors(test_input) == expected
